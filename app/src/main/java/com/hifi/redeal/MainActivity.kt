c63e973dc3b9b89781f9b7155c9f4a7b27baf251
iiolo <alohalo98@naver.com>
2023-09-14 00:13:37 +0900

    design : [지도 화면] 해당 화면 관련 디자인 구현 및 bottomSheet파일 디자인 구현

--- app/src/main/java/com/hifi/redeal/MainActivity.kt
+++ app/src/main/java/com/hifi/redeal/MainActivity.kt
@@ -2,10 +2,86 @@ package com.hifi.redeal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.transition.MaterialSharedAxis
+import com.hifi.redeal.databinding.ActivityMainBinding
+import com.hifi.redeal.map.fragment.MapFragment
+import com.hifi.redeal.schedule.MakeScheduleFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding : ActivityMainBinding
+
+    var newFragment: Fragment? = null
+    var oldFragment:Fragment? = null
+
+
+    companion object{
+        val MAKE_SCHEDULE_FRAGMENT = "MakeScheduleFragment"
+        val MAP_FRAGMENT = "MapFragment"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+//        replaceFragment(MAKE_SCHEDULE_FRAGMENT,false,null)
+        replaceFragment(MAP_FRAGMENT,false,null)
+
+    }
+
+    // 지정한 Fragment를 보여주는 메서드
+    fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle?){
+
+        SystemClock.sleep(200)
+
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        // newFragment 에 Fragment가 들어있으면 oldFragment에 넣어준다.
+        if(newFragment != null){
+            oldFragment = newFragment
+        }
+
+        // 새로운 Fragment를 담을 변수
+        newFragment = when(name){
+            MAKE_SCHEDULE_FRAGMENT -> MakeScheduleFragment()
+            MAP_FRAGMENT -> MapFragment()
+            else -> Fragment()
+        }
+
+        newFragment?.arguments = bundle
+
+        if(newFragment != null) {
+            // 애니메이션 설정
+            if(oldFragment != null){
+                oldFragment?.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+                oldFragment?.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+                oldFragment?.enterTransition = null
+                oldFragment?.returnTransition = null
+            }
+
+            newFragment?.exitTransition = null
+            newFragment?.reenterTransition = null
+            newFragment?.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, true)
+            newFragment?.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
+
+            // Fragment를 교채한다.
+            fragmentTransaction.replace(R.id.mainContainer, newFragment!!)
+
+            if (addToBackStack == true) {
+                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            // 교체 명령이 동작하도록 한다.
+            fragmentTransaction.commit()
+        }
+    }
+
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name:String){
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
