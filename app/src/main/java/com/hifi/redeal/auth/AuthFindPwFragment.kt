593fb1f427676779682a69ca3bcb3e8d2691a7ff
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-09-14 17:29:12 +0900

    feat : 로그인 관련 화면 백버튼 기능 구현

--- app/src/main/java/com/hifi/redeal/auth/AuthFindPwFragment.kt
+++ app/src/main/java/com/hifi/redeal/auth/AuthFindPwFragment.kt
@@ -5,20 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hifi.redeal.MainActivity
 import com.hifi.redeal.R
 import com.hifi.redeal.databinding.FragmentAuthFindPwBinding
 
 class AuthFindPwFragment : Fragment() {
 
     private lateinit var fragmentAuthFindPwBinding: FragmentAuthFindPwBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentAuthFindPwBinding = FragmentAuthFindPwBinding.inflate(inflater)
-        // Inflate the layout for this fragment
+        mainActivity = activity as MainActivity
 
-        return fragmentAuthFindPwBinding.root
+        fragmentAuthFindPwBinding.run {
+            toolbarAuthFindPw.setNavigationOnClickListener {
+                mainActivity.removeFragment(MainActivity.AUTH_FIND_PW_FRAGMENT)
+            }
+
+            return fragmentAuthFindPwBinding.root
+        }
     }
 }
--- app/src/main/java/com/hifi/redeal/auth/AuthJoinFragment.kt
+++ app/src/main/java/com/hifi/redeal/auth/AuthJoinFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hifi.redeal.MainActivity
 import com.hifi.redeal.R
 import com.hifi.redeal.databinding.FragmentAuthFindPwBinding
 import com.hifi.redeal.databinding.FragmentAuthJoinBinding
@@ -13,13 +14,21 @@ import com.hifi.redeal.databinding.FragmentAuthJoinBinding
 class AuthJoinFragment : Fragment() {
 
     private lateinit var fragmentAuthJoinBinding: FragmentAuthJoinBinding
+    lateinit var mainActivity: MainActivity
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentAuthJoinBinding = FragmentAuthJoinBinding.inflate(inflater)
-        // Inflate the layout for this fragment
+        mainActivity = activity as MainActivity
 
-        return fragmentAuthJoinBinding.root
+        fragmentAuthJoinBinding.run {
+            toolbarAuthJoin.setNavigationOnClickListener {
+                mainActivity.removeFragment(MainActivity.AUTH_JOIN_FRAGMENT)
+            }
+
+            return fragmentAuthJoinBinding.root
+        }
     }
 }
