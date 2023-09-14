a85edd6a142a6ba82285dbd5d2f29875724d22fa
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-09-14 13:33:52 +0900

    feat : 로그인 화면 UI 추가

---
+++ app/src/main/java/com/hifi/redeal/auth/AuthFindPwFragment.kt
@@ -0,0 +1,20 @@
+package com.hifi.redeal.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hifi.redeal.R
+
+class AuthFindPwFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_auth_find_pw, container, false)
+    }
+}
