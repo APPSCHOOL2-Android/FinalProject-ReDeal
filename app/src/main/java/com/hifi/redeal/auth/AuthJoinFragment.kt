976adb5d5125bfc5e54931bec7c1c44635fd8576
KHSSE <113084857+KHSSE@users.noreply.github.com>
2023-09-16 22:13:19 +0900

    feat : Authentication 사용 로그인 기능 임시 구현

--- app/src/main/java/com/hifi/redeal/auth/AuthJoinFragment.kt
+++ app/src/main/java/com/hifi/redeal/auth/AuthJoinFragment.kt
@@ -1,20 +1,26 @@
 package com.hifi.redeal.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.hifi.redeal.MainActivity
 import com.hifi.redeal.R
-import com.hifi.redeal.databinding.FragmentAuthFindPwBinding
 import com.hifi.redeal.databinding.FragmentAuthJoinBinding
-
+import com.hifi.redeal.model.UserDataClass
+import com.hifi.redeal.vm.AuthViewModel
 
 class AuthJoinFragment : Fragment() {
 
     private lateinit var fragmentAuthJoinBinding: FragmentAuthJoinBinding
     lateinit var mainActivity: MainActivity
+    private lateinit var authViewModel: AuthViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,13 +28,124 @@ class AuthJoinFragment : Fragment() {
     ): View? {
         fragmentAuthJoinBinding = FragmentAuthJoinBinding.inflate(inflater)
         mainActivity = activity as MainActivity
+        authViewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
 
         fragmentAuthJoinBinding.run {
             toolbarAuthJoin.setNavigationOnClickListener {
                 mainActivity.removeFragment(MainActivity.AUTH_JOIN_FRAGMENT)
             }
 
-            return fragmentAuthJoinBinding.root
+            buttonJoinIdCheck.setOnClickListener {
+
+            }
+
+            buttonJoinComplete.setOnClickListener {
+                val email = textInputEditTextJoinUserId.text.toString()
+                val password = textInputEditTextJoinUserPw.text.toString()
+                val name = textInputEditTextJoinUserName.text.toString()
+                val passwordCheck = textInputEditTextJoinUserPwCheck.text.toString()
+                // 경고 메시지 초기화
+                warningJoinEmailFormat.visibility = View.GONE
+                warningJoinPassword.visibility = View.GONE
+                warningJoinPasswordCheck.visibility = View.GONE
+                warningJoinNicknameFormat.visibility = View.GONE
+                warningJoinEmailAlready.visibility = View.GONE
+                warningJoinNicknameAlready.visibility = View.GONE
+
+                // 예외처리
+                if (!authViewModel.isEmailValid(email)) {
+                    warningJoinEmailFormat.visibility = View.VISIBLE
+                    return@setOnClickListener
+                } else {
+                    warningJoinEmailFormat.visibility = View.GONE
+                }
+
+                if (!authViewModel.isPasswordValid(password)) {
+                    warningJoinPassword.visibility = View.VISIBLE
+                    return@setOnClickListener
+                } else {
+                    warningJoinPassword.visibility = View.GONE
+                }
+
+                if (password != passwordCheck) {
+                    warningJoinPasswordCheck.visibility = View.VISIBLE
+                    return@setOnClickListener
+                } else {
+                    warningJoinPasswordCheck.visibility = View.GONE
+                }
+
+                if (!authViewModel.isNicknameValid(name)) {
+                    warningJoinNicknameFormat.visibility = View.VISIBLE
+                    return@setOnClickListener
+                } else {
+                    warningJoinNicknameFormat.visibility = View.GONE
+                }
+
+                // Firebase Authentication을 사용하여 사용자 등록
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // Firebase Authentication에서 사용자 등록 성공
+                            val firebaseUser = task.result?.user
+
+                            // Firestore에 추가할 사용자 정보 선언
+                            val userData =
+                                UserDataClass(0, email, password, name) // 여기도 UserDataClass로 변경
+
+                            firebaseUser?.uid?.let { uid ->
+                                // Firestore에 사용자 정보 추가 함수 호출
+                                authViewModel.registerUser(email, password, name)
+                            }
+                        } else {
+                            // Firebase Authentication에서 사용자 등록 실패
+                            showErrorMessageDialog("Firebase Authentication에서 사용자 등록 실패: ${task.exception?.message}")
+                        }
+                    }
+
+                // LiveData를 옵저빙하여 결과 처리
+                authViewModel.registrationResult.observe(
+                    viewLifecycleOwner,
+                    Observer { registrationSuccess ->
+                        if (registrationSuccess) {
+                            showRegistrationSuccessDialog()
+                            // 가입 성공 시 화면 이동 로직 추가
+                            mainActivity.replaceFragment(
+                                MainActivity.AUTH_LOGIN_FRAGMENT,
+                                true,
+                                null
+                            )
+                        } else {
+                            showErrorMessageDialog("가입 실패")
+                        }
+                    }
+                )
+            }
         }
+        return fragmentAuthJoinBinding.root
+    }
+
+
+    // 가입 성공 다이얼로그를 보여주는 함수
+    private fun showRegistrationSuccessDialog() {
+        val view =
+            requireActivity().layoutInflater.inflate(R.layout.dialog_join_success_message, null)
+        val alertDialogBuilder = AlertDialog.Builder(requireContext()).setView(view)
+        val alertDialog = alertDialogBuilder.create()
+        val buttonDialogDismiss = view.findViewById<Button>(R.id.buttonJoinSuccess)
+
+        buttonDialogDismiss.setOnClickListener {
+            alertDialog.dismiss()
+        }
+        alertDialog.show()
+    }
+
+    // 오류 처리 다이얼로그를 보여주는 함수
+    private fun showErrorMessageDialog(message: String) {
+        val alertDialogBuilder =
+            AlertDialog.Builder(requireContext()).setTitle("오류").setMessage(message)
+                .setPositiveButton("확인") { dialog, _ -> dialog.dismiss() }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 }
--- app/src/main/java/com/hifi/redeal/vm/AuthViewModel.kt
+++ app/src/main/java/com/hifi/redeal/vm/AuthViewModel.kt
@@ -1,6 +1,7 @@
 package com.hifi.redeal.vm
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
@@ -12,6 +13,10 @@ class AuthViewModel : ViewModel() {
     private val auth = FirebaseAuth.getInstance()
     private val firestore = FirebaseFirestore.getInstance()
     private val userData = MutableLiveData<UserDataClass>()
+
+    // 회원가입 결과를 나타내는 LiveData
+    private val _registrationResult = MutableLiveData<Boolean>()
+    val registrationResult: LiveData<Boolean> = _registrationResult
 
     private val INVALID_NICKNAME_CHARACTERS = listOf(
         "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}",
@@ -26,8 +31,6 @@ class AuthViewModel : ViewModel() {
             if (userUid != null) {
                 AuthRepository.getUserInfoByUserId(userUid) { currentUser ->
                     val userIdx = currentUser.result.child("idx").value as Long
-                    val userEmail = currentUser.result.child("idx").value as String
-                    val userPassword = currentUser.result.child("idx").value as String
                     val userName = currentUser.result.child("idx").value as String
                     val loginUser = UserDataClass(
                         userIdx, email, password, userName
@@ -45,16 +48,33 @@ class AuthViewModel : ViewModel() {
             if (user != null) {
                 getNextIdx { userIdx ->
                     val newUser = UserDataClass(userIdx, email, password, name)
-                    AuthRepository.addUserInfo(newUser) {
-                        Log.d("testloginUserVM", "로그인 성공")
-                    }
-                    userData.value = newUser
+                    addUserToFirestore(user.uid, newUser)
                 }
             } else {
                 // 사용자가 null인 경우 처리
                 Log.d("testloginUserVM", "사용자가 null입니다.")
             }
         }
+    }
+
+    // 파이어스토어에 사용자 정보 추가
+    private fun addUserToFirestore(uid: String, newUser: UserDataClass) {
+        val userData = hashMapOf(
+            "email" to newUser.userEmail,
+            "password" to newUser.userPw,
+            "name" to newUser.userName
+        )
+
+        firestore.collection("users").document(uid)
+            .set(userData)
+            .addOnSuccessListener {
+                // Firestore에 사용자 정보 추가 성공
+                setRegistrationResult(true)
+            }
+            .addOnFailureListener { e ->
+                // Firestore에 사용자 정보 추가 실패
+                showErrorMessageDialog("Firestore에 사용자 정보 추가 실패: ${e.message}")
+            }
     }
 
     // 파이어베이스에서 IDX를 가져와서 인덱스 계산
@@ -64,16 +84,12 @@ class AuthViewModel : ViewModel() {
         firestore.runTransaction { transaction ->
             // 문서를 가져옵니다.
             val docSnapshot = transaction.get(idxRef)
-
             // "value" 필드의 현재 값 또는 기본 값(0)을 가져옵니다.
             val currentIdx = docSnapshot.getLong("value") ?: 0
-
             // 다음 인덱스 계산
             val nextIdx = currentIdx + 1
-
             // 다음 인덱스를 Firestore에 업데이트합니다.
             transaction.update(idxRef, "value", nextIdx)
-
             // 결과를 반환합니다.
             nextIdx
         }.addOnSuccessListener { nextIdx ->
@@ -85,8 +101,16 @@ class AuthViewModel : ViewModel() {
         }
     }
 
+    private fun setRegistrationResult(success: Boolean) {
+        _registrationResult.value = success
+    }
+
     fun isEmailValid(email: String): Boolean {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun showErrorMessageDialog(message: String) {
+        // 오류 처리 다이얼로그를 보여주는 함수 구현
     }
 
     fun isPasswordValid(password: String): Boolean {
