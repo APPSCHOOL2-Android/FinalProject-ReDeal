eed4b2bd4e03885fac659e07df4d517226e32426
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2023-09-19 13:32:54 +0900

    fix : 파일 충돌 수정

--- app/src/main/java/com/hifi/redeal/MainActivity.kt
+++ app/src/main/java/com/hifi/redeal/MainActivity.kt
@@ -12,6 +12,7 @@ import kotlin.concurrent.thread
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context.NOTIFICATION_SERVICE
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -41,6 +42,8 @@ import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.OnBackPressedDispatcher
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
@@ -90,7 +93,6 @@ class MainActivity : AppCompatActivity() {
         val AUTH_LOGIN_FRAGMENT = "AuthLoginFragment"
         val AUTH_JOIN_FRAGMENT = "AuthJoinFragment"
         val AUTH_FIND_PW_FRAGMENT = "AuthFindPwFragment"
-        val MAKE_SCHEDULE_FRAGMENT = "MakeScheduleFragment"
         val MAP_FRAGMENT = "MapFragment"
         val MAP_SEARCH_REGION_FRAGMENT = "MapSearchRegionFragment"
 
@@ -105,8 +107,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
 
     val navOptions = NavOptions.Builder()
-        .setEnterAnim(R.anim.slide_in_right)
-        .setExitAnim(R.anim.slide_out_left)
+        .setEnterAnim(R.anim.slide_out_right)
+        .setExitAnim(R.anim.slide_in_left)
         .setPopEnterAnim(R.anim.slide_in_left)
         .setPopExitAnim(R.anim.slide_out_right)
         .build()
@@ -125,10 +127,6 @@ class MainActivity : AppCompatActivity() {
         addNotificationChannel(NOTIFICATION_CHANNEL1_ID, NOTIFICATION_CHANNEL1_NAME)
         scheduleVM = ViewModelProvider(this)[ScheduleVM::class.java]
 
-    }
-
-    fun replaceFragment(name: String, addToBackStack: Boolean, bundle: Bundle?) {
-
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHostFragmentMain) as NavHostFragment
         navController = navHostFragment.navController
@@ -138,6 +136,7 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
 
     // 지정한 Fragment를 보여주는 메서드
     fun replaceFragment(name:String, addToBackStack:Boolean, bundle:Bundle?){
@@ -193,7 +192,7 @@ class MainActivity : AppCompatActivity() {
             newFragment?.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, false)
 
             // Fragment를 교채한다.
-            fragmentTransaction.replace(R.id.mainContainer, newFragment!!)
+            fragmentTransaction.replace(R.id.navHostFragmentMain, newFragment!!)
 
             if (addToBackStack == true) {
                 // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
@@ -209,45 +208,45 @@ class MainActivity : AppCompatActivity() {
     fun removeFragment(name:String){
         supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
-}
 
-// 입력 요소에 포커스를 주는 메서드
-fun showSoftInput(view: View) {
-    view.requestFocus()
+    // 입력 요소에 포커스를 주는 메서드
+    fun showSoftInput(view: View) {
+        view.requestFocus()
 
-    val inputMethodManger = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    thread {
-        SystemClock.sleep(200)
-        inputMethodManger.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
-    }
-}
-
-
-// Notification Channel을 등록하는 메서드
-// 첫 번째 : 코드에서 채널을 관리하기 위한 이름
-// 두 번째 : 사용자에게 노출 시킬 이름
-fun addNotificationChannel(id: String, name:String){
-    // 안드로이드 8.0 이상일 때만 동작하게 한다.
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-        // 알림 메시지를 관리하는 객체를 추출한다.
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        // id를 통해 NotificationChannel 객체를 추출한다.
-        // 채널이 등록된 적이 없다면 null을 반환한다.
-        val channel = notificationManager.getNotificationChannel(id)
-        // 채널이 등록된 적이 없다면...
-        if(channel == null){
-            // 채널 객체를 생성한다.
-            val newChannel = NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH)
-            // 단말기에 LED 램프가 있다면 램프를 사용하도록 설정한다.
-            newChannel.enableLights(true)
-            // LED 램프의 색상을 설정한다.
-            newChannel.lightColor = Color.BLUE
-            // 진동을 사용할 것인가?
-            newChannel.enableVibration(true)
-            // 채널을 등록한다.
-            notificationManager.createNotificationChannel(newChannel)
+        val inputMethodManger = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        thread {
+            SystemClock.sleep(200)
+            inputMethodManger.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
         }
+    }
 
+    // Notification Channel을 등록하는 메서드
+    // 첫 번째 : 코드에서 채널을 관리하기 위한 이름
+    // 두 번째 : 사용자에게 노출 시킬 이름
+    fun addNotificationChannel(id: String, name:String) {
+        // 안드로이드 8.0 이상일 때만 동작하게 한다.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // 알림 메시지를 관리하는 객체를 추출한다.
+            val notificationManager =
+                getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            // id를 통해 NotificationChannel 객체를 추출한다.
+            // 채널이 등록된 적이 없다면 null을 반환한다.
+            val channel = notificationManager.getNotificationChannel(id)
+            // 채널이 등록된 적이 없다면...
+            if (channel == null) {
+                // 채널 객체를 생성한다.
+                val newChannel = NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH)
+                // 단말기에 LED 램프가 있다면 램프를 사용하도록 설정한다.
+                newChannel.enableLights(true)
+                // LED 램프의 색상을 설정한다.
+                newChannel.lightColor = Color.BLUE
+                // 진동을 사용할 것인가?
+                newChannel.enableVibration(true)
+                // 채널을 등록한다.
+                notificationManager.createNotificationChannel(newChannel)
+            }
+
+        }
     }
 
     fun navigateTo(fragmentId: Int, bundle: Bundle? = null) {
@@ -307,5 +306,6 @@ fun addNotificationChannel(id: String, name:String){
         return dateFormat.format(date)
     }
 
-
 }
+
+
