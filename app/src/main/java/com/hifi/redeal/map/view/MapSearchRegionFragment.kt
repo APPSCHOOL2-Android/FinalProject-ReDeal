b4d6c21d80afbc554ce15e34bfc3a8291c555481
iiolo <alohalo98@naver.com>
2023-09-21 23:33:26 +0900

    feat : 검색어 입력 후 엔터 입력 시 검색어에 해당하는 위치로 지도 보여주는 기능 추가

--- app/src/main/java/com/hifi/redeal/map/view/MapSearchRegionFragment.kt
+++ app/src/main/java/com/hifi/redeal/map/view/MapSearchRegionFragment.kt
@@ -1,11 +1,14 @@
 package com.hifi.redeal.map.view
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
@@ -16,13 +19,16 @@ import com.hifi.redeal.map.adapter.SearchRegionSiDoRecyclerViewAdapter
 import com.hifi.redeal.map.adapter.SearchRegionSiGunGuRecyclerViewAdapter
 import com.hifi.redeal.map.model.AdmVO
 import com.hifi.redeal.map.repository.MapRepository
+import com.hifi.redeal.map.vm.ClientViewModel
 import com.hifi.redeal.map.vm.MapViewModel
+import com.kakao.vectormap.LatLng
 
 class MapSearchRegionFragment : Fragment() {
 
     lateinit var fragmentMapSearchRegionBinding: FragmentMapSearchRegionBinding
     lateinit var mainActivity: MainActivity
     lateinit var mapViewModel: MapViewModel
+    lateinit var clientViewModel : ClientViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,6 +37,7 @@ class MapSearchRegionFragment : Fragment() {
         mainActivity = activity as MainActivity
 
         mapViewModel = ViewModelProvider(mainActivity)[MapViewModel::class.java]
+        clientViewModel = ViewModelProvider(mainActivity)[ClientViewModel::class.java]
 
         mapViewModel.run {
             currentSiGunGuList.observe(mainActivity) {
@@ -50,6 +57,7 @@ class MapSearchRegionFragment : Fragment() {
             }
         }
 
+//        TODO("확인 버튼 클릭 시 현재 선택 되어 있는 지역들을 문자열로 만들어 clientViemodel의 currentAddress에 입력 후 프래그먼트 삭제")
 
         fragmentMapSearchRegionBinding.run {
             mapSearchRegionToolbar.run {
@@ -57,6 +65,24 @@ class MapSearchRegionFragment : Fragment() {
                     mainActivity.removeFragment(MainActivity.MAP_SEARCH_REGION_FRAGMENT)
                 }
             }
+
+            // 서치뷰에서 엔터쳤을 때 동작
+            mapSearchRegionSearchView.setOnQueryTextListener(object :
+                SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    if (query != null) {
+                        setCurrentAddress(query)
+
+                    }
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+            })
+
+
             MapRepository.searchSiDo { result ->
                 mapSearchRegionRecyclerViewSiDo.run {
                     adapter = SearchRegionSiDoRecyclerViewAdapter(mainActivity, result!!.sortedBy { it.admCode })
@@ -72,16 +98,35 @@ class MapSearchRegionFragment : Fragment() {
         return fragmentMapSearchRegionBinding.root
     }
 
-    private fun admVOS(): MutableList<AdmVO> {
-        val admVo1 = AdmVO("경상남도", "48", "경상남도")
-        val admVo2 = AdmVO("경상남도", "47", "경상북도")
-        val admVo3 = AdmVO("전라남도", "46", "전라남도")
-        val admVo4 = AdmVO("전라남도", "45", "전라북도")
-        val admVo5 = AdmVO("충청남도", "44", "충청남도")
-        val admVo6 = AdmVO("충청남도", "42", "강원도")
-        val admVo7 = AdmVO("충청남도", "50", "제주특별자치도")
-        val admList = mutableListOf(admVo1, admVo2, admVo3, admVo4, admVo5, admVo6, admVo7)
-        return admList
+
+
+    fun setCurrentAddress(addr: String) {
+        MapRepository.searchAddr(addr!!) { list ->
+            if (list?.isNotEmpty() == true && list != null) {
+                val lat = list[0].y.toDouble()
+                val long = list[0].x.toDouble()
+                Log.d("주소 확인1", lat.toString())
+                Log.d("주소 확인2", long.toString())
+                clientViewModel.currentAddress.value = LatLng.from(lat,long)
+                mainActivity.removeFragment(MainActivity.MAP_SEARCH_REGION_FRAGMENT)
+            }else{
+                showDialog("주소 오류","지역 명으로 입력해주세요.",mainActivity)
+            }
+        }
     }
+    fun showDialog(title:String,message:String,context: Context) {
+        val alertDialogBuilder = AlertDialog.Builder(context)
+        alertDialogBuilder.setTitle(title)
+        alertDialogBuilder.setMessage(message)
+        alertDialogBuilder.setPositiveButton("확인") { dialog, which ->
+            // 확인 버튼을 클릭했을 때 수행할 동작
+            dialog.dismiss() // 다이얼로그 닫기
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+
 
 }
